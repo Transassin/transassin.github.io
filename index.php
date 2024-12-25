688c244bea432d2bd9f15e416bf8ccdb16819006
Transassin <josie.lee.mason@gmail.0com>
2024-12-24 23:53:58 -0600

    update
    update

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
         <meta name='viewport' content='width=device-width initial-scale=1.0'>
         <link rel='text/html' href='/index.html'>
         <link rel="stylesheet" href="/style.css">
-        <?php get_ ?>
+        <?php
+        
+        ?>
     </head>
     <body background='/assets/3kinifes1kick.mp4'>
             <source src='/assets/3kinifes1kick.mp4'>
